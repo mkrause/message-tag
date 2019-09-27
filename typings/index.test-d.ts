1a3ab6374117d1342a1094ed2dd82395410dff50
mkrause <maikelkrause@gmail.com>
2019-09-27 17:26:50 +0200

    Migrate TypeScript declaration test to use tsd.

--- typings/index.test-d.ts
+++ typings/index.test-d.ts
@@ -1,32 +1,28 @@
 ///<reference lib="es2015"/>
 ///<reference path="./index.d.ts"/>
 
-// Test module to test TypeScript declaration file.
-// Usage:
-//   $ tsc --noEmit --strict --esModuleInterop typings/test.ts
-// See: https://stackoverflow.com/questions/49296151/how-to-write-tests-for-typescript-typing-definition
+// Test module to test TypeScript declaration.
+// Usage: `tsd`.
 
-
+import { expectType, expectError } from 'tsd';
 import msg, { msgTag, raw, custom } from 'message-tag';
 
 
-// None of the following should produce any type errors
+expectType<string>(msg``);
 
-const test1 : string = msg``;
+expectType<string>(msg`foo`);
 
-const test2 : string = msg`foo`;
+expectType<string>(msg`foo ${'bar'}`);
 
-const test3 : string = msg`foo ${'bar'}`;
+expectType<string>(msg`foo ${42}`);
 
-const test4 : string = msg`foo ${42}`;
+expectType<string>(msg`foo ${{ x: 42 }}`);
 
-const test5 : string = msg`foo ${{ x: 42 }}`;
+expectType<string>(msg`foo ${(x : number) => x + 1}`);
 
-const test6 : string = msg`foo ${(x : number) => x + 1}`;
-
-const test7 : string = msg`foo ${msg.raw('bar')}`;
+expectType<string>(msg`foo ${msg.raw('bar')}`);
 
 
 const msgCustom = msg.custom({ format: {} });
 
-const testCustom1 : string = msgCustom`foo ${{ x: 42 }}`;
+expectType<string>(msgCustom`foo ${{ x: 42 }}`);
